8e2b3c88e4b6018c3e0031eb459f66ef6073c22a
mikesaprykin <mikesaprykin@gmail.com>
2017-05-31 09:38:40 +0300

    loader added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import {AfterContentInit, AfterViewInit, Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'app works!';
+
+  ngAfterViewInit() {
+    const loader = document.querySelector('.loader-wrapper');
+    loader.classList.add('hidden');
+    setTimeout(() => {
+      loader.classList.add('none');
+    }, 200)
+  }
 }
